dc66d7e326c75a713183df08bdedebfa10904f10
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-05-18 13:00:46 +0300

    //

--- chat/lib/Screen/login_page.dart
+++ chat/lib/Screen/login_page.dart
@@ -9,7 +9,63 @@ class loginpage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Column(
-        children: [Image.asset('assets/image/MetroUI_Messaging.webp')],
+        children: [
+          SizedBox(height: 230),
+          Center(
+            child: Image.asset(
+              'assets/image/MetroUI_Messaging.webp',
+              height: 200,
+              width: 200,
+              fit: BoxFit.scaleDown,
+            ),
+          ),
+          Text(
+            'Welcome to Chat App',
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+          Text(
+            'Please login to continue',
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+          SizedBox(height: 30),
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Username',
+              labelStyle: TextStyle(color: Colors.black),
+              hintText: 'Enter your username',
+              hintStyle: TextStyle(color: Colors.black),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+                borderSide: BorderSide(color: Colors.black),
+              ),
+            ),
+          ),
+          SizedBox(height: 30),
+          TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: 'Password',
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.black),
+              ),
+              labelStyle: TextStyle(color: Colors.black),
+              hintText: 'Enter your password',
+              hintStyle: TextStyle(color: Colors.black),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+                borderSide: BorderSide(color: Colors.black),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- chat/lib/main.dart
+++ chat/lib/main.dart
@@ -1,16 +1,21 @@
 import 'package:chat/Screen/login_page.dart';
 import 'package:flutter/material.dart';
 
-void mmain() {
-  runApp(const chat());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Future.delayed(Duration(seconds: 3)); // تأخير لعرض السبلاتش شوية
+  runApp(const ChatApp());
 }
 
 // ignore: camel_case_types
-class chat extends StatelessWidget {
-  const chat({super.key});
+class ChatApp extends StatelessWidget {
+  const ChatApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(body: loginpage()));
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(body: loginpage()),
+    );
   }
 }
