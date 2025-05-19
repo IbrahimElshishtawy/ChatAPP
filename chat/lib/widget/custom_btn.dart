7c04d1996f062d1a479d8dd7ad7fd626ce816427
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-05-19 15:00:50 +0300

    //

--- chat/lib/widget/custom_btn.dart
+++ chat/lib/widget/custom_btn.dart
@@ -2,24 +2,30 @@ import 'package:flutter/material.dart';
 
 // ignore: must_be_immutable
 class CustomBtn extends StatelessWidget {
-  CustomBtn({super.key, this.textbtn, required Null Function() onPressed});
-  String? textbtn;
+  final String textbtn;
+  final VoidCallback onPressed;
+
+  const CustomBtn({super.key, required this.textbtn, required this.onPressed});
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 380,
-      height: 60,
-      decoration: BoxDecoration(
-        color: const Color.fromARGB(255, 60, 155, 227),
-        borderRadius: BorderRadius.circular(24),
-      ),
-      child: Center(
-        child: Text(
-          '$textbtn',
-          style: const TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        width: 380,
+        height: 60,
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 60, 155, 227),
+          borderRadius: BorderRadius.circular(24),
+        ),
+        child: Center(
+          child: Text(
+            textbtn,
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
           ),
         ),
       ),
