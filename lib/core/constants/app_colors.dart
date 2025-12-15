6ae8aa401a8096037b7ba5465ab444d51026365e
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-15 19:50:28 +0200

    Update app_colors.dart

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Primary
+  static const Color primary = Color(0xFF2C688E);
+  static const Color secondary = Color(0xFF4FA3D1);
+
+  // Light
+  static const Color lightBackground = Color(0xFFF5F7FA);
+  static const Color lightCard = Colors.white;
+  static const Color lightText = Color(0xFF1E1E1E);
+
+  // Dark
+  static const Color darkBackground = Color(0xFF121212);
+  static const Color darkCard = Color(0xFF1E1E1E);
+  static const Color darkText = Colors.white;
+
+  // Status
+  static const Color success = Colors.green;
+  static const Color danger = Colors.red;
+}
