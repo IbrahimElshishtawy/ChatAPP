bba453a1a1df182c2b478d5845e3a34918cc7f11
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-15 19:30:25 +0200

    Update user_controller.dart

---
+++ lib/controllers/user/user_controller.dart
@@ -0,0 +1,38 @@
+import 'package:get/get.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../../core/models/user_model.dart';
+import '../../core/services/user_service.dart';
+
+class UserController extends GetxController {
+  final UserService _service = UserService();
+  Rx<UserModel?> user = Rx<UserModel?>(null);
+
+  @override
+  void onInit() {
+    final uid = FirebaseAuth.instance.currentUser?.uid;
+    if (uid != null) {
+      loadUser(uid);
+    }
+    super.onInit();
+  }
+
+  Future<void> loadUser(String uid) async {
+    user.value = await _service.getUser(uid);
+  }
+
+  Future<void> updateProfile(String name) async {
+    if (user.value == null) return;
+    final updated = UserModel(
+      id: user.value!.id,
+      name: name,
+      email: user.value!.email,
+      role: user.value!.role,
+    );
+    await _service.updateUser(updated);
+    user.value = updated;
+  }
+
+  void clear() {
+    user.value = null;
+  }
+}
