1115806330919539206657a24fd5b80df130594b
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-15 19:31:36 +0200

    Update splash_page.dart

--- lib/screens/splash/splash_page.dart
+++ lib/screens/splash/splash_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../../controllers/auth/auth_controller.dart';
-import '../../app/routes.dart';
+import '../../app/routes/routes.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
