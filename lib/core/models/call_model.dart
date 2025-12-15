5d3d728a90b3fa5c98cdceb4ca768cfc00b8e932
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-15 22:35:34 +0200

    Update call_model.dart

--- lib/core/models/call_model.dart
+++ lib/core/models/call_model.dart
@@ -1,32 +1,43 @@
 enum CallType { voice, video }
 
-enum CallStatus { ringing, accepted, rejected, ended }
+enum CallStatus { ringing, accepted, rejected, missed, ended }
 
 class CallModel {
   final String callId;
   final String callerId;
   final String receiverId;
+  final String channelName;
   final CallType type;
   final CallStatus status;
-  final String channelName;
+  final DateTime createdAt;
 
   CallModel({
     required this.callId,
     required this.callerId,
     required this.receiverId,
+    required this.channelName,
     required this.type,
     required this.status,
-    required this.channelName,
+    required this.createdAt,
   });
 
-  factory CallModel.fromMap(Map<String, dynamic> map) {
-    return CallModel(
-      callId: map['callId'],
-      callerId: map['callerId'],
-      receiverId: map['receiverId'],
-      channelName: map['channelName'],
-      type: CallType.values.byName(map['type']),
-      status: CallStatus.values.byName(map['status']),
-    );
-  }
+  Map<String, dynamic> toMap() => {
+    'callId': callId,
+    'callerId': callerId,
+    'receiverId': receiverId,
+    'channelName': channelName,
+    'type': type.name,
+    'status': status.name,
+    'createdAt': createdAt,
+  };
+
+  factory CallModel.fromMap(Map<String, dynamic> map) => CallModel(
+    callId: map['callId'],
+    callerId: map['callerId'],
+    receiverId: map['receiverId'],
+    channelName: map['channelName'],
+    type: CallType.values.byName(map['type']),
+    status: CallStatus.values.byName(map['status']),
+    createdAt: map['createdAt'].toDate(),
+  );
 }
