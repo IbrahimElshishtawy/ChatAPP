4c1f440eff9779a400cb7ef014f261ae07628501
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-15 19:50:26 +0200

    Create theme_controller.dart

---
+++ lib/controllers/theme/theme_controller.dart
@@ -0,0 +1,14 @@
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import '../../core/theme/app_theme.dart';
+
+class ThemeController extends GetxController {
+  RxBool isDark = false.obs;
+
+  ThemeMode get themeMode => isDark.value ? ThemeMode.dark : ThemeMode.light;
+
+  void toggleTheme() {
+    isDark.value = !isDark.value;
+    Get.changeTheme(isDark.value ? AppTheme.dark : AppTheme.light);
+  }
+}
