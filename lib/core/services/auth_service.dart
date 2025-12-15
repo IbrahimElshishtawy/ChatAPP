d28bc7b3870868a9ed6c6df91de6a241bfb7dc71
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-15 18:52:03 +0200

    Update auth_service.dart

---
+++ lib/core/services/auth_service.dart
@@ -0,0 +1,36 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Stream<User?> authStateChanges() => _auth.authStateChanges();
+
+  Future<User?> login(String email, String password) async {
+    final cred = await _auth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    return cred.user;
+  }
+
+  Future<User?> register({
+    required String email,
+    required String password,
+    required Map<String, dynamic> userData,
+  }) async {
+    final cred = await _auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+
+    await _firestore.collection('users').doc(cred.user!.uid).set(userData);
+
+    return cred.user;
+  }
+
+  Future<void> logout() async {
+    await _auth.signOut();
+  }
+}
