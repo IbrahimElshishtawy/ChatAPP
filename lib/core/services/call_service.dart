f32a808460027521a4e1be3435eb6a013186079f
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-15 22:35:37 +0200

    Update call_service.dart

--- lib/core/services/call_service.dart
+++ lib/core/services/call_service.dart
@@ -3,10 +3,11 @@ import '../models/call_model.dart';
 
 class CallService {
   final _firestore = FirebaseFirestore.instance;
-
+  final _calls = FirebaseFirestore.instance.collection('calls');
   CollectionReference calls() => _firestore.collection('calls');
 
   Future<void> startCall(CallModel call) async {
+    await _calls.doc(call.callId).set(call.toMap());
     await calls().doc(call.callId).set({
       'callId': call.callId,
       'callerId': call.callerId,
@@ -27,5 +28,22 @@ class CallService {
 
   Future<void> updateCallStatus(String callId, CallStatus status) async {
     await calls().doc(callId).update({'status': status.name});
+  }
+
+  Future<void> updateStatus(String callId, CallStatus status) async {
+    await _calls.doc(callId).update({'status': status.name});
+  }
+
+  Stream<List<CallModel>> callHistory(String uid) {
+    return _calls
+        .where(
+          Filter.or(
+            Filter('callerId', isEqualTo: uid),
+            Filter('receiverId', isEqualTo: uid),
+          ),
+        )
+        .orderBy('createdAt', descending: true)
+        .snapshots()
+        .map((s) => s.docs.map((d) => CallModel.fromMap(d.data())).toList());
   }
 }
