3d91d0f6c6b6ffe77cec7ebaacfc98b6eb852187
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-15 19:50:16 +0200

    Update app.dart

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,19 +1,26 @@
+import 'package:chat/app/routes/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'routes/routes.dart';
-import 'bindings/bindings.dart';
+import '../controllers/theme/theme_controller.dart';
+import '../core/theme/app_theme.dart';
 
 class ChatApp extends StatelessWidget {
   const ChatApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Chat App',
-      initialBinding: InitialBinding(),
-      initialRoute: AppRoutes.splash,
-      getPages: AppRoutes.pages,
+    final themeCtrl = Get.find<ThemeController>();
+
+    return Obx(
+      () => GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Chat App',
+        theme: AppTheme.light,
+        darkTheme: AppTheme.dark,
+        themeMode: themeCtrl.themeMode,
+        initialRoute: AppRoutes.splash,
+        getPages: AppRoutes.pages,
+      ),
     );
   }
 }
