fddebad9b8354bb01b53f8afafbfa99823c58369
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-16 16:33:04 +0200

    Update bindings.dart

--- lib/app/bindings/bindings.dart
+++ lib/app/bindings/bindings.dart
@@ -1,15 +1,19 @@
+import 'package:chat/controllers/call/Call_History_Controller.dart';
 import 'package:chat/controllers/call/call_controller.dart';
+import 'package:chat/controllers/chat/chat_controller.dart';
 import 'package:chat/controllers/navigation/navigation_controller.dart';
+import 'package:chat/controllers/settings/settings_controller.dart';
+import 'package:chat/controllers/theme/theme_controller.dart';
 import 'package:get/get.dart';
-import '../../controllers/auth/auth_controller.dart';
-import '../../controllers/user/user_controller.dart';
 
 class InitialBinding extends Bindings {
   @override
   void dependencies() {
-    Get.put(AuthController(), permanent: true);
-    Get.put(UserController(), permanent: true);
+    Get.put(ThemeController(), permanent: true);
     Get.put(NavigationController(), permanent: true);
+    Get.put(ChatController(), permanent: true);
     Get.put(CallController(), permanent: true);
+    Get.put(CallHistoryController(), permanent: true);
+    Get.put(SettingsController(), permanent: true);
   }
 }
