bc85c30b3d25b6533d1af5eef1224c1ea98d3506
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-16 16:33:10 +0200

    Create Call_History_Controller.dart

---
+++ lib/controllers/call/Call_History_Controller.dart
@@ -0,0 +1,23 @@
+import 'package:get/get.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../../core/services/call_service.dart';
+import '../../core/models/call_model.dart';
+
+class CallHistoryController extends GetxController {
+  final CallService _service = CallService();
+
+  final RxList<CallModel> calls = <CallModel>[].obs;
+
+  String get currentUserId => FirebaseAuth.instance.currentUser!.uid;
+
+  @override
+  void onInit() {
+    super.onInit();
+    calls.bindStream(_service.callHistory(currentUserId));
+  }
+
+  bool isOutgoing(CallModel call) => call.callerId == currentUserId;
+
+  bool isMissed(CallModel call) =>
+      call.status == CallStatus.missed && call.receiverId == currentUserId;
+}
