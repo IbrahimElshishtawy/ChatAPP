152c9dddc76af3eec46dc0a9033ea0bb123ad4a1
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-17 00:09:27 +0200

    Create auth_bindings.dart

---
+++ lib/app/bindings/auth_bindings.dart
@@ -0,0 +1,9 @@
+import 'package:chat/screens/auth/widget/login_form_controller.dart';
+import 'package:get/get.dart';
+
+class AuthBindings extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<LoginFormController>(() => LoginFormController(), fenix: true);
+  }
+}
