ba9dbda36f92e732ea02540a9365183431cb0d2f
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-16 16:33:13 +0200

    Update theme_controller.dart

--- lib/controllers/theme/theme_controller.dart
+++ lib/controllers/theme/theme_controller.dart
@@ -1,9 +1,9 @@
-import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import '../../core/theme/app_theme.dart';
 
 class ThemeController extends GetxController {
-  RxBool isDark = false.obs;
+  final RxBool isDark = false.obs;
 
   ThemeMode get themeMode => isDark.value ? ThemeMode.dark : ThemeMode.light;
 
