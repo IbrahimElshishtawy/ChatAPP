c3f6df4fdc069760825574e6824194978fc74eb0
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-16 18:55:27 +0200

    Update bindings.dart

--- lib/app/bindings/bindings.dart
+++ lib/app/bindings/bindings.dart
@@ -1,22 +1,27 @@
-import 'package:chat/controllers/call/Call_History_Controller.dart';
-import 'package:chat/controllers/call/call_controller.dart';
-import 'package:chat/controllers/chat/chat_controller.dart';
-import 'package:chat/controllers/navigation/navigation_controller.dart';
-import 'package:chat/controllers/settings/settings_controller.dart';
-import 'package:chat/controllers/theme/theme_controller.dart';
 import 'package:get/get.dart';
+
+import '../../controllers/auth/auth_controller.dart';
+import '../../controllers/theme/theme_controller.dart';
+import '../../controllers/navigation/navigation_controller.dart';
+import '../../controllers/chat/chat_controller.dart';
+import '../../controllers/call/call_controller.dart';
+import '../../controllers/call/call_history_controller.dart';
+import '../../controllers/settings/settings_controller.dart';
 
 class InitialBinding extends Bindings {
   @override
   void dependencies() {
+    // 🔐 لازم يبقى أول Controller
+    Get.put(AuthController(), permanent: true);
+
+    // 🎨 Navigation & Theme
     Get.put(ThemeController(), permanent: true);
     Get.put(NavigationController(), permanent: true);
-    Get.put(ChatController(), permanent: true);
-    Get.put(CallController(), permanent: true);
-    Get.put(CallHistoryController(), permanent: true);
-    Get.put(SettingsController(), permanent: true);
+
+    // 💬 الباقي lazy
     Get.lazyPut(() => ChatController(), fenix: true);
     Get.lazyPut(() => CallController(), fenix: true);
     Get.lazyPut(() => CallHistoryController(), fenix: true);
+    Get.lazyPut(() => SettingsController(), fenix: true);
   }
 }
