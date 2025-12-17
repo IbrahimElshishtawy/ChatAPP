18c80408287005552cb1ee77ba9b2fb40e2e9170
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-17 17:16:06 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,10 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-//@pragma('vm:entry-point')
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {}
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
