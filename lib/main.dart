1e77aa8d55859f7f8516536fffde5a47bf24a470
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-17 19:17:14 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:chat/app/app.dart';
+import 'package:chat/core/services/notification_service.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
@@ -12,6 +13,6 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-
+  NotificationService().initAndSaveToken();
   runApp(const ChatApp());
 }
