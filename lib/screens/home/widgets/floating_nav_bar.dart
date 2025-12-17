1048aedaa6ed5f8e10447ad8837b387362308b07
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-18 00:32:23 +0200

    Update floating_nav_bar.dart

--- lib/screens/home/widgets/floating_nav_bar.dart
+++ lib/screens/home/widgets/floating_nav_bar.dart
@@ -1,4 +1,3 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../../../controllers/navigation/navigation_controller.dart';
@@ -10,89 +9,85 @@ class FloatingNavBar extends StatelessWidget {
   Widget build(BuildContext context) {
     final nav = Get.find<NavigationController>();
 
-    return Positioned(
-      left: 20,
-      right: 20,
-      bottom: 20,
-      child: Obx(() {
-        return ClipRRect(
-          borderRadius: BorderRadius.circular(28),
-          child: BackdropFilter(
-            filter: ImageFilter.blur(sigmaX: 12, sigmaY: 12),
-            child: Container(
-              height: 64,
-              padding: const EdgeInsets.symmetric(horizontal: 12),
-              decoration: BoxDecoration(
-                color: Colors.black.withOpacity(0.35),
-                borderRadius: BorderRadius.circular(28),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.25),
-                    blurRadius: 24,
-                    offset: const Offset(0, 10),
-                  ),
-                ],
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  _item(Icons.chat, 0, 'Chats'),
-                  _item(Icons.group, 1, 'Groups'),
-                  _item(Icons.public, 2, 'Community'),
-                  _item(Icons.notifications, 3, 'Alerts'),
-                  _item(Icons.person, 4, 'Profile'),
-                ],
-              ),
-            ),
-          ),
-        );
-      }),
-    );
-  }
-
-  Widget _item(IconData icon, int i, String label) {
-    final nav = Get.find<NavigationController>();
-
     return Obx(() {
-      final selected = nav.index.value == i;
+      final current = nav.index.value;
 
-      return GestureDetector(
-        onTap: () => nav.change(i),
-        child: AnimatedContainer(
-          duration: const Duration(milliseconds: 300),
-          curve: Curves.easeOutBack,
-          padding: EdgeInsets.symmetric(
-            horizontal: selected ? 18 : 12,
-            vertical: 10,
-          ),
+      return Positioned(
+        bottom: 20,
+        left: 20,
+        right: 20,
+        child: Container(
+          height: 64,
           decoration: BoxDecoration(
-            color: selected ? Colors.white : Colors.transparent,
-            borderRadius: BorderRadius.circular(20),
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(24),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.12),
+                blurRadius: 20,
+                offset: const Offset(0, 10),
+              ),
+            ],
           ),
           child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              AnimatedScale(
-                scale: selected ? 1.2 : 1,
-                duration: const Duration(milliseconds: 250),
-                child: Icon(
-                  icon,
-                  color: selected ? Colors.black : Colors.white,
-                ),
+              _item(
+                icon: Icons.chat,
+                index: 0,
+                current: current,
+                onTap: () => nav.change(0),
               ),
-              if (selected) ...[
-                const SizedBox(width: 6),
-                Text(
-                  label,
-                  style: const TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              ],
+              _item(
+                icon: Icons.group,
+                index: 1,
+                current: current,
+                onTap: () => nav.change(1),
+              ),
+              _item(
+                icon: Icons.public,
+                index: 2,
+                current: current,
+                onTap: () => nav.change(2),
+              ),
+              _item(
+                icon: Icons.notifications,
+                index: 3,
+                current: current,
+                onTap: () => nav.change(3),
+              ),
+              _item(
+                icon: Icons.person,
+                index: 4,
+                current: current,
+                onTap: () => nav.change(4),
+              ),
             ],
           ),
         ),
       );
     });
+  }
+
+  Widget _item({
+    required IconData icon,
+    required int index,
+    required int current,
+    required VoidCallback onTap,
+  }) {
+    final active = index == current;
+
+    return GestureDetector(
+      onTap: onTap,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 250),
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: active ? Colors.blue.withOpacity(0.12) : Colors.transparent,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Icon(icon, size: 26, color: active ? Colors.blue : Colors.grey),
+      ),
+    );
   }
 }
