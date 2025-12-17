5cc2ec4d65668f1bb259c6cebc362c63f03f53f4
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-18 00:39:52 +0200

    Update user_service.dart

--- lib/core/services/user_service.dart
+++ lib/core/services/user_service.dart
@@ -2,15 +2,23 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import '../models/user_model.dart';
 
 class UserService {
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final _users = FirebaseFirestore.instance.collection('users');
 
+  /// 🔹 Get single user
   Future<UserModel?> getUser(String uid) async {
-    final doc = await _firestore.collection('users').doc(uid).get();
+    final doc = await _users.doc(uid).get();
     if (!doc.exists) return null;
     return UserModel.fromMap(doc.id, doc.data()!);
   }
 
+  /// 🔹 UPDATE user
   Future<void> updateUser(UserModel user) async {
-    await _firestore.collection('users').doc(user.id).update(user.toMap());
+    await _users.doc(user.id).update(user.toMap());
+  }
+
+  /// 🔥 GET ALL USERS (دي الجديدة)
+  Future<List<UserModel>> getAllUsers() async {
+    final snap = await _users.get();
+    return snap.docs.map((e) => UserModel.fromMap(e.id, e.data())).toList();
   }
 }
