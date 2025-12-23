6f7254c78dd123aed1132e221a66f2aecb044e78
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-23 23:10:15 +0200

    Update auth_controller.dart

--- lib/controllers/auth/auth_controller.dart
+++ lib/controllers/auth/auth_controller.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unnecessary_null_comparison
+
 import 'package:chat/core/services/notification_service.dart';
 import 'package:get/get.dart';
 import 'package:firebase_auth/firebase_auth.dart';
