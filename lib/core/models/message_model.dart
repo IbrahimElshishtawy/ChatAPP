128cbd04d14d2325c305e974683b007d2c0fd830
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-24 00:23:56 +0200

    Update message_model.dart

--- lib/core/models/message_model.dart
+++ lib/core/models/message_model.dart
@@ -1,34 +1,31 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class MessageModel {
-  final String id;
-  final String senderId;
   final String text;
+  final String senderId;
   final DateTime createdAt;
   final bool isSeen;
 
   MessageModel({
-    required this.id,
-    required this.senderId,
     required this.text,
+    required this.senderId,
     required this.createdAt,
     required this.isSeen,
   });
 
   Map<String, dynamic> toMap() {
     return {
-      'senderId': senderId,
       'text': text,
-      'createdAt': Timestamp.fromDate(createdAt),
+      'senderId': senderId,
+      'createdAt': createdAt,
       'isSeen': isSeen,
     };
   }
 
-  factory MessageModel.fromDoc(String id, Map<String, dynamic> map) {
+  factory MessageModel.fromMap(Map<String, dynamic> map) {
     return MessageModel(
-      id: id,
+      text: map['text'] ?? '',
       senderId: map['senderId'],
-      text: map['text'],
       createdAt: (map['createdAt'] as Timestamp).toDate(),
       isSeen: map['isSeen'] ?? false,
     );
