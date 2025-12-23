39c909df4579e376fd89659028bab5364158cd75
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-23 20:49:39 +0200

    Update notification_controller.dart

--- lib/controllers/notification/notification_controller.dart
+++ lib/controllers/notification/notification_controller.dart
@@ -2,8 +2,7 @@ import 'package:get/get.dart';
 import '../../core/services/notification_service.dart';
 
 class NotificationController extends GetxController {
-  final NotificationService _service = NotificationService();
-
+  final NotificationService _service = NotificationService.instance;
   @override
   void onInit() {
     _service.initAndSaveToken();
