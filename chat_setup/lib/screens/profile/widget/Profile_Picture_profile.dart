ed761ca13a3db9e4137a1b5736f8221712bc88ba
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-01-01 01:24:04 +0200

    Update Profile_Picture_profile.dart

--- chat_setup/lib/screens/profile/widget/Profile_Picture_profile.dart
+++ chat_setup/lib/screens/profile/widget/Profile_Picture_profile.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:chat_setup/controllers/user/user_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
@@ -22,14 +21,52 @@ class ProfilePicture extends StatelessWidget {
           await userCtrl.updateProfilePicture(File(image.path));
         }
       },
-      child: CircleAvatar(
-        radius: 50,
-        backgroundImage: userCtrl.user.value?.profilePicture != null
-            ? NetworkImage(userCtrl.user.value!.profilePicture!)
-            : const AssetImage('assets/default_avatar.png') as ImageProvider,
-        child: userCtrl.user.value?.profilePicture == null
-            ? const Icon(Icons.camera_alt, color: Colors.white)
-            : null,
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          Container(
+            height: 100,
+            width: 100,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(color: Colors.blueAccent, width: 3),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.1),
+                  blurRadius: 10,
+                  offset: Offset(0, 5),
+                ),
+              ],
+            ),
+            child: CircleAvatar(
+              radius: 50,
+              backgroundImage: userCtrl.user.value?.profilePicture != null
+                  ? NetworkImage(userCtrl.user.value!.profilePicture!)
+                  : const AssetImage('assets/default_avatar.png')
+                        as ImageProvider,
+              child: userCtrl.user.value?.profilePicture == null
+                  ? const Icon(Icons.camera_alt, color: Colors.white)
+                  : null,
+            ),
+          ),
+          Positioned(
+            right: 0,
+            bottom: 0,
+            child: IconButton(
+              icon: const Icon(Icons.edit, color: Colors.blue, size: 24),
+              onPressed: () async {
+                final ImagePicker picker = ImagePicker();
+                final XFile? image = await picker.pickImage(
+                  source: ImageSource.gallery, // تغيير الصورة
+                );
+
+                if (image != null) {
+                  await userCtrl.updateProfilePicture(File(image.path));
+                }
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
