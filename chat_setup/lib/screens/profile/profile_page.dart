b74d2e59664db396fa55ca58a4b2a7cce0b0bf38
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2025-12-31 18:49:11 +0200

    Update profile_page.dart

--- chat_setup/lib/screens/profile/profile_page.dart
+++ chat_setup/lib/screens/profile/profile_page.dart
@@ -1,5 +1,14 @@
+import 'dart:io';
+import 'package:chat_setup/screens/profile/widget/Complete_Profile_Button.dart';
+import 'package:chat_setup/screens/profile/widget/Contact_Button_profile.dart';
+import 'package:chat_setup/screens/profile/widget/Description_user_profile.dart';
+import 'package:chat_setup/screens/profile/widget/Profile_Picture_profile.dart';
+import 'package:chat_setup/screens/profile/widget/Share_pprofile_Button.dart';
+import 'package:chat_setup/screens/profile/widget/Stats_Section_profile.dart';
+import 'package:chat_setup/screens/profile/widget/User_Info_profile.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 import '../../controllers/user/user_controller.dart';
 
 class ProfilePage extends StatelessWidget {
@@ -11,36 +20,61 @@ class ProfilePage extends StatelessWidget {
     final nameCtrl = TextEditingController(
       text: userCtrl.user.value?.name ?? '',
     );
+    final phoneCtrl = TextEditingController(
+      text: userCtrl.user.value?.phone ?? '',
+    );
+    final descriptionCtrl = TextEditingController(
+      text: userCtrl.user.value?.description ?? '',
+    );
+    final usernameCtrl = TextEditingController(
+      text: userCtrl.user.value?.username ?? '',
+    );
 
     return Scaffold(
-      appBar: AppBar(title: const Text('Profile')),
+      appBar: AppBar(
+        title: const Text('Profile'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.edit),
+            onPressed: () {
+              Get.toNamed('/editProfile');
+            },
+          ),
+        ],
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Obx(() {
           final user = userCtrl.user.value;
           if (user == null) return const CircularProgressIndicator();
 
+          bool isProfileComplete =
+              user.name.isNotEmpty &&
+              user.phone != null &&
+              user.username != null &&
+              user.description != null &&
+              user.profilePicture != null;
+
           return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              TextField(
-                controller: nameCtrl,
-                decoration: const InputDecoration(labelText: 'Name'),
+              ProfilePicture(userCtrl: userCtrl),
+              const SizedBox(height: 6),
+              StatsSection(),
+              const SizedBox(height: 16),
+              UserInfo(
+                nameCtrl: nameCtrl,
+                phoneCtrl: phoneCtrl,
+                usernameCtrl: usernameCtrl,
               ),
               const SizedBox(height: 16),
-              SizedBox(
-                width: double.infinity,
-                height: 48,
-                child: ElevatedButton(
-                  onPressed: () async {
-                    await userCtrl.updateProfile(nameCtrl.text.trim());
-                    Get.snackbar('Success', 'Profile updated');
-                  },
-                  child: const Text('Save'),
-                ),
-              ),
-              const SizedBox(height: 12),
-              Text('Email: ${user.email}'),
-              Text('Role: ${user.role}'),
+              DescriptionSection(descriptionCtrl: descriptionCtrl),
+              const SizedBox(height: 16),
+              if (!isProfileComplete) CompleteProfileButton(),
+              const SizedBox(height: 16),
+              ShareButton(),
+              const SizedBox(height: 16),
+              ContactButton(),
             ],
           );
         }),
