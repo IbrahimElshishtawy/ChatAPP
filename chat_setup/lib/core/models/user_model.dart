d385eddeebf42cdf3ebe693dd9f493149706fad3
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2025-12-31 18:48:52 +0200

    Update user_model.dart

--- chat_setup/lib/core/models/user_model.dart
+++ chat_setup/lib/core/models/user_model.dart
@@ -4,13 +4,33 @@ class UserModel {
   final String? email;
   final String? phone;
   final String role;
+  final String? linkedin;
+  final String? facebook;
+  final String? instagram;
+  final String? whatsapp;
+  final String backgroundImage;
+  final String? username;
+  final String? profilePicture;
+  final String? description; // وصف المستخدم
+  final bool freeTrial; // حالة الفترة التجريبية
+  final bool paymentStatus; // حالة الدفع
 
   UserModel({
+    required this.profilePicture,
     required this.id,
     required this.name,
     this.email,
     this.phone,
     required this.role,
+    this.username,
+    this.linkedin,
+    this.facebook,
+    this.instagram,
+    this.whatsapp,
+    required this.backgroundImage,
+    this.description,
+    this.freeTrial = true,
+    this.paymentStatus = false,
   });
 
   factory UserModel.fromMap(String id, Map<String, dynamic> map) {
@@ -20,11 +40,28 @@ class UserModel {
       email: map['email'],
       phone: map['phone'],
       role: map['role'] ?? 'user',
+      username: map['username'],
+      profilePicture: map['profilePicture'] ?? '',
+      backgroundImage: map['backgroundImage'] ?? '',
+      description: map['description'],
+      freeTrial: map['freeTrial'] ?? true,
+      paymentStatus: map['paymentStatus'] ?? false,
     );
   }
 
   Map<String, dynamic> toMap() {
-    return {'name': name, 'email': email, 'phone': phone, 'role': role};
+    return {
+      'name': name,
+      'email': email,
+      'phone': phone,
+      'role': role,
+      'username': username,
+      'profilePicture': profilePicture,
+      'backgroundImage': backgroundImage,
+      'description': description,
+      'freeTrial': freeTrial,
+      'paymentStatus': paymentStatus,
+    };
   }
 
   UserModel copyWith({
@@ -32,6 +69,16 @@ class UserModel {
     String? email,
     String? phone,
     String? role,
+    String? username,
+    String? profilePicture,
+    String? linkedin,
+    String? facebook,
+    String? instagram,
+    String? whatsapp,
+    String? backgroundImage,
+    String? description,
+    bool? freeTrial,
+    bool? paymentStatus,
   }) {
     return UserModel(
       id: id,
@@ -39,6 +86,16 @@ class UserModel {
       email: email ?? this.email,
       phone: phone ?? this.phone,
       role: role ?? this.role,
+      profilePicture: profilePicture ?? this.profilePicture,
+      username: username ?? this.username,
+      linkedin: linkedin ?? this.linkedin,
+      facebook: facebook ?? this.facebook,
+      instagram: instagram ?? this.instagram,
+      whatsapp: whatsapp ?? this.whatsapp,
+      backgroundImage: backgroundImage ?? this.backgroundImage,
+      description: description ?? this.description,
+      freeTrial: freeTrial ?? this.freeTrial,
+      paymentStatus: paymentStatus ?? this.paymentStatus,
     );
   }
 }
